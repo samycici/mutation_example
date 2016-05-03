9d0ff00d60f8a276da27ecf56689c5a71f13b065
Samanta Cecília de Barros Souza <samanta@MAC7448.local>
2016-05-03 10:52:48 -0300

    Alter return

--- discount.rb
+++ discount.rb
@@ -1,10 +1,10 @@
 class Discount
   def get_discount(price)
     if price <= 0
-      return 'Value not allowed'
+      "Value not allowed"
     elsif price >0 and price <100
       discount = 0
-    else price >=100 
+    else price >=100
       discount = 30
     end
   end
